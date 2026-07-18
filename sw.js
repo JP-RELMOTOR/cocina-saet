3de523e6f68d27eea7ce1355e497958866db0f66
JP-RELMOTOR <jp.relmotor@gmail.com>
2026-07-17 22:01:15 -0400

    deploy: auto-update

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    - Libs externas (cdn) -> CACHE-FIRST (no cambian)
    La versión de CACHE se genera SOLA con el hash del contenido cacheado
    (scripts/bump-sw-version.sh, llamado por el hook de deploy). No editar a mano. */
-const CACHE = "cocina-saet-7ad180c48c";
+const CACHE = "cocina-saet-e653681c7f";
 const PRECACHE = ["./", "./index.html", "./manifest.json", "./recetas-data.js",
   "./icon-192.png", "./icon-512.png", "./logo.png",
   "./img/montaje-almuerzo-1.jpg", "./img/montaje-almuerzo-2.jpg",
